32152f30a0dbc57cd4d7667b48dc736b88a39ffe
felipe-almarques <108095964+felipe-almarques@users.noreply.github.com>
2023-08-23 19:46:36 -0300

    Create exercicio4.R

---
+++ Lista3/exercicio4.R
@@ -0,0 +1,14 @@
+#4. Utilize o dataset HTV, estime o modelo de regresÃ£o
+#educ = Î²0 + Î²1motheduc + Î²2fatheduc + Î²3abil + Î²4abil2 + u
+#e interprete os resultados.
+
+install.packages("wooldridge")
+library(wooldridge)
+data(htv)
+view(htv)
+
+modelo_htv <- lm(educ ~ motheduc + fatheduc + abil + I(abil^2), data = htv)
+coef(modelo_htv)
+anova(modelo_htv)
+summary(modelo_htv)
+
